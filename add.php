bdf08b9f09892e0eef82167f58152ae4381cf621
Tatiana Panteleeva <tatiana.panteleeva2018@gmail.com>
2021-08-04 15:27:38 +1000

    Server side vadidation on presence field's values, checking data type of each field

--- add.php
+++ add.php
@@ -3,42 +3,78 @@ require 'data.php';
 require 'functions.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	$lot = $_POST;
+    $lot = $_POST;
+    $lot_photo = $_FILES['lot-photo'];
 
-	$required = ['title', 'description'];
-	$dict = ['title' => 'Name', 'description' => 'Description', 'file' => 'Image'];
-	$errors = [];
-	foreach ($required as $key) {
-		if (empty($_POST[$key])) {
-            $errors[$key] = 'Fill up this field';
+    $required_fields = ['lot-name', 'category', 'message', 'lot-rate', 'lot-step', 'lot-date'];
+    $errors = [];
+
+    foreach ($required_fields as $field) {
+		if (empty($_POST[$field])) {
+            $errors[$field] = 'Fill up this field';
 		}
 	}
 
-	if (isset($_FILES['lot_img']['name'])) {
+    foreach ($_POST as $key=>$value) {
+        if ($key=== 'lot-rate') {
+            if($value!==""&&!filter_var($value, FILTER_VALIDATE_INT)) {
+                $errors['lot-rate'] = 'Insert integer';
+            }
+        }
+
+        if ($key === 'lot-step') {
+            if($value!==""&&!filter_var($value, FILTER_VALIDATE_INT)) {
+                $errors['lot-step'] = 'Insert integer';
+            }
+        }
+
+        if ($key === 'lot-name') {
+            if ($value!==""&&!preg_match('/^[a-zA-Z0-9-_]+$/', $value)) {
+                $errors['lot-name'] = 'should contain only alphanumeric!';
+            }
+        }
+
+        if ($key === 'message') {
+            if ($value!==""&&!preg_match('/^[a-zA-Z0-9-_]+$/', $value)) {
+                $errors['message'] = 'should contain only alphanumeric!';
+            }
+        }
+    }
+
+    if (isset($_FILES['lot_img']['name'])) {
 		$tmp_name = $_FILES['lot_img']['tmp_name'];
 		$path = $_FILES['lot_img']['name'];
 
 		$finfo = finfo_open(FILEINFO_MIME_TYPE);
 		$file_type = finfo_file($finfo, $tmp_name);
-		if ($file_type !== "image/JPEG") {
-			$errors['file'] = 'Upload photo in JP';
+		if ($file_type !== "image/jpeg") {
+			$errors['file'] = 'Upload photo in jpeg format';
 		}
 		else {
-			move_uploaded_file($tmp_name, 'uploads/' . $path);
-			$lotf['path'] = $path;
+			move_uploaded_file($tmp_name, 'img/' . $path);
+			$lot['path'] = $path;
 		}
 	}
+
 	else {
-		$errors['file'] = 'You have not uploaded image';
+		$errors['file'] = 'You have not uploaded file jpeg';
 	}
 
-	if (count($errors)) {
-		$page_content = includeTemplate('add.php', ['lot' => $lot, 'errors' => $errors, 'dict' => $dict]);
-	}
-	else {
-		$page_content = includeTemplate('view.php', ['lot' => $lot]);
-	}
+    if (count($errors)) {
+        $page_content = includeTemplate('add.php', [
+            'lot' => $lot,
+            'errors' => $errors,
+            'categories' => $categories,
+        ]);
+    }
+    else {
+        $page_content = includeTemplate('lot.php', [
+            'categories' => $categories,
+            'lot' => $lot,
+        ]);
+    }
 }
+
 else {
 	$page_content = includeTemplate('add.php', [
         'categories' => $categories,
--- templates/add.php
+++ templates/add.php
@@ -1,3 +1,12 @@
+<?php
+$lot_name = $lot['lot-name'] ?? '';
+$category = $lot['category'] ?? '';
+$message  = $lot['message']  ?? '';
+$lot_rate = $lot['lot-rate'] ?? '';
+$lot_step = $lot['lot-step'] ?? '';
+$lot_date = $lot['lot-date'] ?? '';
+?>
+
 <main>
     <nav class="nav">
         <ul class="nav__list container">
@@ -8,66 +17,69 @@
             <?php endforeach?>
         </ul>
     </nav>
-    <form class="form form--add-lot container form--invalid" action="./add.php" method="post">
-        <!-- form--invalid -->
+
+    <form class="form form--add-lot container <?= isset($errors) ? 'form--invalid' : '' ?>"
+        enctype="multipart/form-data" action="add.php" method="post">
         <h2>Adding lot</h2>
         <div class="form__container-two">
-            <div class="form__item form__item--invalid">
-                <!-- form__item--invalid -->
+            <div class="form__item <?= isset($errors['lot-name']) ? 'form__item--invalid' : '' ?>">
                 <label for="lot-name">Name</label>
-                <input id="lot-name" type="text" name="lot-name" placeholder="Name your lot" required>
-                <span class="form__error">Name your lot</span>
+                <input id="lot-name" type="text" name="lot-name" placeholder="Enter lot name"
+                    value="<?= $lot_name ?>" />
+                <span class="form__error"><?= $errors['lot-name'] ?? '' ?></span>
             </div>
-            <div class="form__item">
+            <div class="form__item <?= isset($errors['category']) ? 'form__item--invalid' : '' ?>">
                 <label for="category">Category</label>
-                <select id="category" name="category" required>
-                    <option>Choose category</option>
+                <select id="category" name="category">
+                    <option selected><?= $category ?></option>
                     <?php foreach($categories as $key => $value): ?>
                     <option><?=$value['category']?></option>
                     <?php endforeach?>
                 </select>
-                <span class="form__error">Choose category</span>
+                <span class="form__error"><?= $errors['category'] ?? '' ?></span>
             </div>
         </div>
-        <div class="form__item form__item--wide">
+        <div class="form__item form__item--wide <?= isset($errors['message']) ? 'form__item--invalid' : '' ?>">
             <label for="message">Description</label>
-            <textarea id="message" name="message" placeholder="Describe your lot" required></textarea>
-            <span class="form__error">Describe your lot</span>
+            <textarea id="message" name="message" placeholder="Enter lot description"><?= $message ?></textarea>
+            <!-- required -->
+            <span class="form__error"><?= $errors['message'] ?? '' ?></span>
         </div>
-        <div class="form__item form__item--file">
+        <div class="form__item form__item--file <?= isset($errors['file']) ? 'form__item--invalid' : '' ?>">
             <!-- form__item--uploaded -->
             <label>Photo</label>
             <div class="preview">
                 <button class="preview__remove" type="button">x</button>
                 <div class="preview__img">
-                    <img src="img/avatar.jpg" width="113" height="113" alt="Lot photo">
+                    <img src="img/avatar.jpg" width="113" height="113" alt="Photo of lot" />
                 </div>
             </div>
             <div class="form__input-file">
-                <input class="visually-hidden" type="file" id="photo2" value="">
+                <input class="visually-hidden" type="file" id="photo2" name="lot-photo" />
                 <label for="photo2">
                     <span>+ Add</span>
                 </label>
             </div>
+            <span class="form__error"><?= $errors['file'] ?? '' ?></span>
         </div>
         <div class="form__container-three">
-            <div class="form__item form__item--small">
-                <label for="lot-rate">Start price</label>
-                <input id="lot-rate" type="number" name="lot-rate" placeholder="0" required>
-                <span class="form__error">Insert start price</span>
+            <div class="form__item form__item--small <?= isset($errors['lot-rate']) ? 'form__item--invalid' : '' ?>">
+                <label for="lot-rate">Start bid</label>
+                <input id="lot-rate" type="number" name="lot-rate" placeholder="0" value="<?= $lot_rate ?>" />
+                <span class="form__error"><?= $errors['lot-rate'] ?? '' ?></span>
             </div>
-            <div class="form__item form__item--small">
+            <div class="form__item form__item--small <?= isset($errors['lot-step']) ? 'form__item--invalid' : '' ?>">
                 <label for="lot-step">Bid step</label>
-                <input id="lot-step" type="number" name="lot-step" placeholder="0" required>
-                <span class="form__error">Bid step</span>
+                <input id="lot-step" type="number" name="lot-step" placeholder="0" value="<?= $lot_step ?>" />
+                <span class="form__error"><?= $errors['lot-step'] ?? '' ?></span>
             </div>
-            <div class="form__item">
-                <label for="lot-date">Finish date for bids</label>
-                <input class="form__input-date" id="lot-date" type="date" name="lot-date" required>
-                <span class="form__error">Insert finish date for making bids</span>
+            <div class="form__item <?= isset($errors['lot-date']) ? 'form__item--invalid' : '' ?>">
+                <label for="lot-date">Auction final date</label>
+                <input class="form__input-date" id="lot-date" type="date" name="lot-date" value="<?= $lot_date ?>" />
+                <span class="form__error"><?= $errors['lot-date'] ?? '' ?></span>
             </div>
         </div>
-        <span class="form__error form__error--bottom">Please, correct mistakes in form</span>
-        <button type="submit" class="button">Submit lot</button>
+        <span class="form__error form__error--bottom">Please, correct mistakes in your form</span>
+        <button type="submit" class="button">Upload lot</button>
     </form>
 </main>
