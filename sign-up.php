fb825234f9aa98b3265bf7826f820c8184a47d2a
Tatiana Panteleeva <tatiana.panteleeva2018@gmail.com>
2021-08-16 12:28:25 +1000

    sign-up Form validation

--- sign-up.php
+++ sign-up.php
@@ -1,12 +1,86 @@
 <?php
-require ('config.php');
-require ('functions.php');
-require ('data.php');
+require 'data.php';
+require 'functions.php';
+require 'config.php';
 
-$page_content = includeTemplate('sign-up.php', [
-    'categories' => $categories,
-    'lots' => $lots,
-]);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $form = $_POST;
+    $user_photo = $_FILES['user-photo'];
+
+    $required_fields = ['email', 'password', 'name', 'message'];
+    $errors = [];
+
+    foreach ($form as $field => $value) {
+        if ($field == 'email') {
+            if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                $errors[$field] = 'Email has to be correct';
+            }
+        }
+
+        if ($field=== 'password') {
+            if (!preg_match('/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}/', $value)) {
+                $errors['password'] = 'Must contain at least one number and one uppercase and lowercase letter, and at least 5 or more characters';
+            }
+        }
+
+        if ($field === 'name') {
+            if (!preg_match('/^[a-zA-Z0-9-_]+$/', $value)) {
+                $errors['name'] = 'should contain only alphanumeric!';
+            }
+        }
+
+        if ($field === 'message') {
+            if (!preg_match('/^[a-zA-Z0-9-_]+$/', $value)) {
+                $errors['message'] = 'should contain only alphanumeric!';
+            }
+        }
+
+        if (empty($value)) {
+            $errors[$field] = 'Fill up this field';
+        }
+    }
+
+    if ($_FILES['user-photo']['name']) {
+        if (isset($_FILES['user-photo']['name'])) {
+            $tmp_name = $user_photo['tmp_name'];
+            $path = $user_photo['name'];
+
+            $finfo = finfo_open(FILEINFO_MIME_TYPE);
+            $file_type = finfo_file($finfo, $tmp_name);
+
+            if ($file_type !== "image/jpeg") {
+                $errors['file'] = 'Upload photo in jpeg format';
+            }
+            else {
+                move_uploaded_file($tmp_name, 'img/' . $path);
+                $form['path'] = $path;
+            }
+        }
+    }
+     else {
+		$errors['file'] = 'You have not uploaded';
+	}
+
+	if (count($errors)) {
+		$page_content = includeTemplate('sign-up.php', [
+            'categories' => $categories,
+            'form' => $form,
+            'errors' => $errors,
+        ]);
+	}
+    else {
+		$page_content = includeTemplate('index.php', [
+            'form' => $form,
+            'categories' => $categories,
+        ]);
+	}
+}
+
+else {
+	$page_content = includeTemplate('sign-up.php', [
+        'categories' => $categories,
+    ]);
+}
 
 $layout_content = includeTemplate('layout.php', [
     'content' => $page_content,
--- templates/sign-up.php
+++ templates/sign-up.php
@@ -1,3 +1,9 @@
+<?php
+$email = $form['email'] ?? '';
+$password = $form['password'] ?? '';
+$name = $form['name'] ?? '';
+$message  = $form['message']  ?? '';
+?>
 <main>
     <nav class="nav">
         <ul class="nav__list container">
@@ -8,31 +14,33 @@
             <?php endforeach?>
         </ul>
     </nav>
-    <form class="form container" action="https://echo.htmlacademy.ru" method="post">
+    <form class="form container <?= isset($errors) ? 'form--invalid' : '' ?>" enctype="multipart/form-data"
+        action="sign-up.php" method="post">
         <!-- form--invalid -->
         <h2>Register new accaunt</h2>
-        <div class="form__item">
+        <div class="form__item <?= isset($errors['email']) ? 'form__item--invalid' : '' ?>">
             <!-- form__item--invalid -->
             <label for="email">E-mail*</label>
-            <input id="email" type="text" name="email" placeholder="Insert e-mail" required>
-            <span class="form__error">Insert e-mail</span>
+            <input id="email" type="text" name="email" value="<?= $email;?>" placeholder="Insert e-mail">
+            <span class="form__error"><?= $errors['email'] ?? '' ?></span>
         </div>
-        <div class="form__item">
+        <div class="form__item <?= isset($errors['password']) ? 'form__item--invalid' : '' ?> ">
             <label for="password">Password*</label>
-            <input id="password" type="text" name="password" placeholder="Insert e-mail" required>
-            <span class="form__error">Insert password</span>
+            <input id="password" type="text" name="password" value="<?= $password;?>" placeholder="Insert password">
+            <span class="form__error"><?= $errors['password'] ?? '' ?></span>
         </div>
-        <div class="form__item">
+        <div class="form__item <?= isset($errors['name']) ? 'form__item--invalid' : '' ?>">
             <label for="name">Name*</label>
-            <input id="name" type="text" name="name" placeholder="Insert your name" required>
-            <span class="form__error">Your name</span>
+            <input id="name" type="text" name="name" value="<?= $name;?>" placeholder="Insert your name">
+            <span class="form__error"><?= $errors['name'] ?? '' ?></span>
         </div>
-        <div class="form__item">
+        <div class="form__item <?= isset($errors['message']) ? 'form__item--invalid' : '' ?>">
             <label for="message">Contact details*</label>
-            <textarea id="message" name="message" placeholder="Your contact details" required></textarea>
-            <span class="form__error">Your contact details</span>
+            <textarea id="message" name="message" placeholder="Your contact details"><?= $message;?></textarea>
+            <span class="form__error"><?= $errors['message'] ?? '' ?></span>
         </div>
-        <div class="form__item form__item--file form__item--last">
+        <div
+            class="form__item form__item--file form__item--last <?= isset($errors['file']) ? 'form__item--invalid' : '' ?>">
             <label>Your photo</label>
             <div class="preview">
                 <button class="preview__remove" type="button">x</button>
@@ -41,13 +49,14 @@
                 </div>
             </div>
             <div class="form__input-file">
-                <input class="visually-hidden" type="file" id="photo2" value="">
-                <label for="photo2">
+                <input class="visually-hidden" type="file" id="photo" name="user-photo">
+                <label for="photo">
                     <span>+ Add</span>
                 </label>
+                <span class="form__error"><?= $errors['file'] ?? '' ?></span>
             </div>
         </div>
-        <span class="form__error form__error--bottom">Please correct mistakes in the form.</span>
+        <span class="form__error form__error--bottom">Please, correct mistakes in your form</span>
         <button type="submit" class="button">Register</button>
 
         <a class="text-link" href="#">Already have this accaunt</a>
