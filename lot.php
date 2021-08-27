e49cfbcd05c4dbc89508cb9a5b7343af0c6b3260
Tatiana Panteleeva <tatiana.panteleeva2018@gmail.com>
2021-08-27 14:10:33 +1000

    rendering individual lot from DB, rendering static list of bids, fixing queries

--- lot.php
+++ lot.php
@@ -1,49 +1,97 @@
 <?php
-    require 'data.php';
     require 'functions.php';
     require 'config.php';
+    require 'init.php';
 
     $lot = null;
     $cookie_name = 'lot_history';
     $lot_indices = [];
     $cookie_expire = strtotime("+30 days");
     $cookie_path = "/";
+    $lot_id = $_GET['id'] ? intval($_GET['id']) : null;
 
-    if (isset($_GET['lot_id'])) {
-        $lot_id = $_GET['lot_id'];
+    if (!$link) {
+        $error = mysqli_connect_error();
+        $page_content = include_template('error.php', ['error' => $error]);
+    } else {
+        $sqlCategories = 'SELECT `CategoryName`, `CategoryClass` FROM categories';
+        $resultCategories = mysqli_query($link, $sqlCategories);
 
-        foreach ($lots as $key => $value) {
-            if ($key == $lot_id) {
-                $lot = $value;
-                break;
-            }
+        if ($resultCategories) {
+            $categories = mysqli_fetch_all($resultCategories, MYSQLI_ASSOC);
         }
 
-        if (isset($_COOKIE['lot_history'])) {
-            $lot_indices = unserialize($_COOKIE['lot_history']);
+        else {
+            $error = mysqli_error($link);
+            $page_content = include_template('error.php', ['error' => $error]);
+        }
 
-            if (!in_array($lot_id, $lot_indices)) {
+       $sqlLots = 'SELECT *  FROM Lots as l
+       JOIN Categories AS c ON l.CategoryID=c.CategoryID
+       ORDER BY `LotDateTime` DESC LIMIT 9';
+
+        if ($res = mysqli_query($link, $sqlLots)) {
+            $lots = mysqli_fetch_all($res, MYSQLI_ASSOC);
+        }
+        else {
+            $page_content = include_template('error.php', ['error' => mysqli_error($link)]);
+        }
+
+        $sqlBids= "SELECT * FROM Bids as b
+        JOIN Users AS u ON b.UserID=u.UserID
+        WHERE b.LotID = '1'
+        ORDER BY BidPrice DESC";
+
+        if ($res = mysqli_query($link, $sqlBids )) {
+            $bids = mysqli_fetch_all($res, MYSQLI_ASSOC);
+        }
+        else {
+            $page_content = include_template('error.php', ['error' => mysqli_error($link)]);
+        }
+
+        $page_content = include_template('index.php', [
+            'categories' => $categories,
+            'lots' => $lots,
+            'bids' => $bids,
+        ]);
+
+        if (isset($_GET['lot_id'])) {
+            $lot_id = $_GET['lot_id'];
+
+            foreach ($lots as $key => $value) {
+                if ($key == $lot_id) {
+                    $lot = $value;
+                    break;
+                }
+            }
+
+            if (isset($_COOKIE['lot_history'])) {
+                $lot_indices = unserialize($_COOKIE['lot_history']);
+
+                if (!in_array($lot_id, $lot_indices)) {
+                    $lot_indices[] = $lot_id;
+                }
+
+                $cookie_value = serialize($lot_indices);
+                setcookie($cookie_name , $cookie_value, $cookie_expire, $cookie_path);
+            } else {
                 $lot_indices[] = $lot_id;
+                $cookie_value = serialize($lot_indices);
+                setcookie($cookie_name , $cookie_value, $cookie_expire, $cookie_path);
             }
-
-            $cookie_value = serialize($lot_indices);
-            setcookie($cookie_name , $cookie_value, $cookie_expire, $cookie_path);
-        } else {
-            $lot_indices[] = $lot_id;
-            $cookie_value = serialize($lot_indices);
-            setcookie($cookie_name , $cookie_value, $cookie_expire, $cookie_path);
         }
-    }
 
-    if (!$lot) {
-        exit(http_response_code(404));
+        if (!$lot) {
+            exit(http_response_code(404));
+        }
+
     }
 
     $page_content = include_template('lot.php', [
         'categories' => $categories,
         'lots' => $lot,
         'is_auth' => $is_auth,
-        'Bids' => $Bids,
+        'bids' => $bids,
     ]);
 
     $layout_content = include_template('layout.php', [
